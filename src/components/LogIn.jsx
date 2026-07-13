78d61d6385344423a6966c5cccd4191549786ca6
BarbieDollx <barbiedoll6605@gmail.com>
2026-07-13 13:37:38 +0100

    others

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import "./Login.css";
+import "./LogIn.css";
 
 const Login = () => {
   const navigate = useNavigate();
