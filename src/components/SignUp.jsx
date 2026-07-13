be30bc85362c0ec4a147aec3d58278462b035346
BarbieDollx <barbiedoll6605@gmail.com>
2026-07-13 13:00:34 +0100

    signup

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import "./Signup.css";
+import "./SignUp.css";
 
 const Signup = () => {
   const navigate = useNavigate();
